5d10f4f5af0be1aef9abcb81228064ff6b58f9a4
Giovanna Pança <pancagiovanna@gmail.com>
2026-04-13 19:09:06 -0300

    correcao pipeline

--- test_blog.py
+++ test_blog.py
@@ -10,7 +10,7 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())), options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 def test_search_valid_keyword():
     driver = webdriver.Chrome()
