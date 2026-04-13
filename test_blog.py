d4a2df9a0189fae38c9cffada52c24361dca8d63
Giovanna Pança <pancagiovanna@gmail.com>
2026-04-13 19:00:04 -0300

    correcao

--- test_blog.py
+++ test_blog.py
@@ -1,7 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
 
 def test_search_valid_keyword():
     driver = webdriver.Chrome()
