0e8fa3134f2377a3787a6b85d82fbb49ffdac538
Giovanna Pança <pancagiovanna@gmail.com>
2026-04-14 12:08:41 -0300

    upgrade test

--- test_blog.py
+++ test_blog.py
@@ -8,37 +8,35 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def create_driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless") 
-    chrome_options.add_argument("--no-sandbox")  
-    chrome_options.add_argument("--disable-dev-shm-usage")  
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    return driver
-
+    return webdriver.Chrome(service=service, options=chrome_options)
 
 def test_search_valid_keyword():
-    driver = webdriver.Chrome()
-    driver.get("https://blogdoagi.com.br/")
-    
-    search_box = driver.find_element(By.NAME, "s")  # Localiza a caixa de pesquisa
-    search_box.send_keys("tecnologia")  # Insira uma palavra-chave válida
-    search_box.send_keys(Keys.RETURN)  # Pressiona Enter
-    
-    time.sleep(2)  # Aguarde os resultados carregarem
-    assert "Resultados da pesquisa" in driver.page_source  # Verifica se a página contém a frase esperada
-    driver.quit()
+    driver = create_driver()
+    try:
+        driver.get("https://blogdoagi.com.br/")
+        search_box = driver.find_element(By.NAME, "s")
+        search_box.send_keys("tecnologia")
+        search_box.send_keys(Keys.RETURN)
+        time.sleep(2)
+        assert "Resultados da pesquisa" in driver.page_source
+    finally:
+        driver.quit()
 
 def test_search_invalid_keyword():
-    driver = webdriver.Chrome()
-    driver.get("https://blogdoagi.com.br/")
-    
-    search_box = driver.find_element(By.NAME, "s")  # Localiza a caixa de pesquisa
-    search_box.send_keys("xyz123")  # Insira uma palavra-chave inválida
-    search_box.send_keys(Keys.RETURN)  # Pressiona Enter
-    
-    time.sleep(2)  # Aguarde os resultados carregarem
-    assert "Nenhum resultado encontrado" in driver.page_source  # Verifica se a página contém a frase esperada
-    driver.quit()
+    driver = create_driver()
+    try:
+        driver.get("https://blogdoagi.com.br/")
+        search_box = driver.find_element(By.NAME, "s")
+        search_box.send_keys("xyz123")
+        search_box.send_keys(Keys.RETURN)
+        time.sleep(2)
+        assert "Nenhum resultado encontrado" in driver.page_source
+    finally:
+        driver.quit()
 
 if __name__ == "__main__":
     test_search_valid_keyword()
